15fdbfa5dc7e76dd16fc38bc7264231706b191ae
Malek Hamza <malek.hamza@gmx.de>
2023-01-15 15:17:07 +0100

    put navbar in own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { QualificationListComponent } from './qualification-list/qualification-list.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeListComponent,
-    QualificationListComponent
+    QualificationListComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
