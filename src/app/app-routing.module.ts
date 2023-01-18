6a0d46e59fa285d708992ce6e1777fa7169c6fdb
Malek Hamza <malek.hamza@gmx.de>
2023-01-18 10:33:21 +0100

    Routed components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeEditorComponent } from './employee-editor/employee-editor.component';
 import{EmployeeListComponent} from "./employee-list/employee-list.component";
+import { QualificationDetailsComponent } from './qualification-details/qualification-details.component';
+import { QualificationEditorComponent } from './qualification-editor/qualification-editor.component';
 import { QualificationListComponent } from './qualification-list/qualification-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/employee', pathMatch: 'full' },
   { path: 'employee', component: EmployeeListComponent},
+  { path: 'employee/:id', component: EmployeeDetailsComponent },
+  { path: 'employee/editor/:id', component: EmployeeEditorComponent },
+  { path: 'qualification/:id', component: QualificationDetailsComponent },
+  { path: 'qualification/editor/:id', component: QualificationEditorComponent },
   { path: 'qualification', component: QualificationListComponent},
 ];
 
