4d58065829afc9eb4075d343d19eecfd4de56464
Malek Hamza <malek.hamza@gmx.de>
2023-01-19 20:10:43 +0100

    added tsdocs to employee service

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -18,25 +18,54 @@ export class EmployeeService {
     this.bearer = authService.getToken();
   }
 
+  /**
+   * Gets all Employees from Service
+   * 
+   * @returns Employees as Observable
+   */
   public getEmployees(): Observable<Employee[]> {
     return this.http.get<Employee[]>('/backend/employees', {
       headers: this.getHeaders()
     });
   }
 
+  /**
+   * Gets asynchronously an employee by its id.
+   * 
+   * @param id of the employee
+   * @returns employee as promise
+   */
   public async getEmployeeById(id: number): Promise<Employee> {
     return await firstValueFrom(this.http.get<Employee>(`/backend/employees/${id}`, {
       headers: this.getHeaders()
     }));
   }
+
+  /**
+   * Adds asynchronously an employee.
+   * 
+   * @param employee object to add
+   * @returns employee callback as promise
+   */
   public async addEmployee(employee: Employee): Promise<Employee> {
     return firstValueFrom(this.http.post('/backend/employyes', employee, {headers: this.getHeaders()}))
   }
 
+  /**
+   * Edits asynchronously an employee object.
+   * 
+   * @param employee to edit with matching id
+   * @returns employee callback as promise
+   */
   public async editEmployee(employee: Employee): Promise<Employee> {
     return firstValueFrom(this.http.put(`/backend/employees/${employee.id}`, employee, {headers: this.getHeaders()}))
   }
 
+  /**
+   * Gets required Headers for Employee related requests
+   * 
+   * @returns headers for requests
+   */
   private getHeaders(): HttpHeaders | { [header: string]: string | string[]; } | undefined {
     return new HttpHeaders()
       .set('Content-Type', 'application/json')
