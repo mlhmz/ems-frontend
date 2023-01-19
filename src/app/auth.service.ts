1b8e40a2321efe20b5b535ee437da17fcad811f1
Malek Hamza <malek.hamza@gmx.de>
2023-01-19 18:20:44 +0100

    wrote tsdoc for auth service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -15,22 +15,47 @@ export class AuthService {
 
   private readonly tokenKey = 'token';
 
-  public getToken() : string {
+  /**
+   * Gets token as string from cookies
+   * 
+   * @returns token as string
+   */
+  public getToken(): string {
     return this.cookieService.get(this.tokenKey);
   }
 
-  public isTokenAvailable() : boolean {
+  /**
+   * Checks if a token is available and not empty in the cookies of the browser;
+   * 
+   * @returns boolean if the token is available
+   */
+  public isTokenAvailable(): boolean {
     return this.cookieService.check(this.tokenKey) && this.isTokenNotEmpty();
   }
 
-  private isTokenNotEmpty() {
+  /**
+   * Checks if the token is not empty
+   * 
+   * @returns boolean if token string is not empty
+   */
+  private isTokenNotEmpty(): boolean {
     return this.cookieService.get(this.tokenKey).length != 0;
   }
 
+  /**
+   * Clears the login token on log out.
+   */
   public clearToken() {
     this.cookieService.delete(this.tokenKey);
   }
 
+  /**
+   * Fetches asynchronously the Token from the Authentication Service
+   * 
+   * @param username The username of the certain user
+   * @param password The password of the certain user
+   * @returns callback boolean if the login was successful as promise
+   */
   public async fetchToken(username: string, password: string): Promise<boolean> {
     return await firstValueFrom(this.http.post("/auth", this.getLoginParams(username, password), this.getUrlEncodedOptions()))
     .then((res: any) => {
@@ -42,22 +67,41 @@ export class AuthService {
     })
   }
 
+  /**
+   * Saves the Token to the cookies of the browser.
+   */
   private saveToken(res: any) {
     this.cookieService.set(this.tokenKey, res.access_token, this.getExpirationDate(), "/");
   }
 
+  /**
+   * Gets expiration date of the token.
+   * The Date will be generated and saved as UTC, but the browser itself will adjust it
+   * to the Timezone of the User.
+   * 
+   * @returns The expiration date
+   */
   private getExpirationDate(): number | Date | undefined {
     let date = new Date();
     date.setHours(date.getHours() + 1);
     return date;
   }
 
+  /**
+   * Gets required HttpHeaders for Auth related requests
+   */
   private getUrlEncodedOptions() {
     return {
       headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
     };
   }
 
+  /**
+   * Generates Parameters for Logging into the authentication service
+   * 
+   * @param username The username of the certain user
+   * @param password The password of the certain user
+   */
   private getLoginParams(username: string, password: string) {
     return new HttpParams()
       .set('grant_type', 'password')
